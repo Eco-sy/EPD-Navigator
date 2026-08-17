6f0ac538776fdfe96ba3bfe2139328399cb59f05
FitchInks <fitchinks@gmail.com>
2026-08-17 15:51:14 +0200

    testing script to resize WIX-iframe

---
+++ resizeReporter.js
@@ -0,0 +1,15 @@
+(function () {
+  const target = document.getElementById("app");
+  const WIX_ORIGIN = "https://www.eco-sy.com/";
+
+  function reportHeight() {
+    const height = target.scrollHeight;
+    window.parent.postMessage({ type: "epd-calc-resize", height }, WIX_ORIGIN);
+  }
+
+  const observer = new ResizeObserver(() => reportHeight());
+  observer.observe(target);
+
+  // Einmal initial senden, sobald alles geladen ist
+  window.addEventListener("load", reportHeight);
+})();
